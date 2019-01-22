1445cfdd03d347fd93cc8496a8e4c5747c53a520
Morgan Sorensen <morgansorensen@mail.weber.edu>
2019-01-22 09:03:39 -0700

    Initial commit

---
+++ chars.c
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  chars.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  01/22/2019 08:54:31 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Morgan Sorensen (), morgansorensen@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+/ Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    int age = 20;
+    double weight = 178.3;
+    char sex = 'M'; // Use single ticks
+
+    printf("Your age is %d and you weight %lf and your sex is %c\n",
+            age, weight, sex);
+
+    return 0;
+}
+// Function Definitions
+
+

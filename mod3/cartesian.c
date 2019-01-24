820cf734fff590a072b11af242addea866de8528
Morgan Sorensen <morgansorensen@mail.weber.edu>
2019-01-24 08:43:06 -0700

    Initial commit

---
+++ mod3/cartesian.c
@@ -0,0 +1,41 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  cartesian.c
+ *
+ *    Description:  Calculate the cartesian coordinates based on :
+ *                  radius and theta angle (degrees)
+ *
+ *        Version:  1.0
+ *        Created:  01/24/2019 08:35:53 AM
+ *       Revision:  none
+ *       Compiler:  gcc cartesian.c -o cartesian.out -lm
+ *
+ *         Author:  Morgan Sorensen (), morgansorensen@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    // 1) Capture user input for radius and theta angle (degrees)
+    // 2) Convert the angle to radians
+    // 3) Calculate your cartesian coordinates
+    // 4) Print the result
+
+    
+
+
+    return 0;
+}
+// Function Definitions
+
+

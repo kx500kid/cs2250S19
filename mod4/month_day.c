096e8224b481a8c6d1bc38185f2395ade1b8abce
Morgan Sorensen <morgansorensen@mail.weber.edu>
2019-01-31 09:20:30 -0700

    Initial commit

---
+++ mod4/month_day.c
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  month_day.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  01/31/2019 09:18:18 AM
+ *       Revision:  none
+ *       Compiler:  gcc month_day.c -o
+ *
+ *         Author:  Morgan Sorensen (), morgansorensen@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    // 1) Capture user input
+
+    // 2) Use IF to validate month
+    
+    // 3) Use SWITCH to validate day of the month
+
+    return 0;
+}
+// Function Definitions
+
+
